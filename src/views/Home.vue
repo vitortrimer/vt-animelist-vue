7eb585a034c1778646459669d0212edfbbd835ad
Vitor Trimer <vitortrimer@gmail.com>
2020-10-21 16:48:58 -0300

    fix anime name

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
           <div class="featured--horizontal">
             <div class="featured--content">
               <div class="featured--content--title">
-                Kimetsu no Yayba
+                Kimetsu no Yaiba
               </div>
               <span class="featured--content--synopsis">
                 Ever since the death of his father, the burden of supporting the family has fallen upon Tanjirou Kamado's shoulders. Though living impoverished on a remote mountain, the Kamado family are able to enjoy a relatively peaceful and happy life. One day, Tanjirou decides to go down to the local village to make a little money selling charcoal. On his way back, night falls, forcing Tanjirou to take shelter in the house of a strange man, who warns him of the existence of flesh-eating demons that lurk in the woods at night. When he finally arrives back home the next day, he is met with a horrifying sight—his whole family has been slaughtered. Worse still, the sole survivor is his sister Nezuko, who has been turned into a bloodthirsty demon. Consumed by rage and hatred, Tanjirou swears to avenge his family and stay by his only remaining sibling. Alongside the mysterious group calling themselves the Demon Slayer Corps, Tanjirou will do whatever it takes to slay the demons and protect the remnants of his beloved sister's humanity.
